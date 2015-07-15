56828f2fc299bf6f2607a5b5db214b1be37ba7fd
Kris McGinnes <kmcginnes@me.com>
2015-07-15 10:19:13 -0500

    Added beginnings of syntax highlighting

--- src/ChaseTheTail/DocumentView.xaml.cs
+++ src/ChaseTheTail/DocumentView.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
+using System.Windows.Media;
+using System.Xml;
+using ICSharpCode.AvalonEdit.Highlighting;
 using ReactiveUI;
 
 namespace ChaseTheTail
@@ -11,6 +15,28 @@ namespace ChaseTheTail
             InitializeComponent();
 
             this.OneWayBind(ViewModel, vm => vm.Content, v => v.Content.Text);
+
+            // Load our custom highlighting definition
+            IHighlightingDefinition customHighlighting;
+            using (var stream = typeof(DocumentView).Assembly.GetManifestResourceStream("ChaseTheTail.LogFile.xshd"))
+            {
+                if (stream == null)
+                    throw new InvalidOperationException("Could not find embedded resource");
+                using (XmlReader reader = new XmlTextReader(stream))
+                {
+                    customHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.
+                        HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
+            // and register it in the HighlightingManager
+            HighlightingManager.Instance.RegisterHighlighting(
+                "LogFile Highlighting", 
+                new [] { ".log" }, 
+                customHighlighting);
+
+            this.SetValue(TextOptions.TextFormattingModeProperty, TextFormattingMode.Display);
+
+            Content.SyntaxHighlighting = customHighlighting;
         }
 
         object IViewFor.ViewModel
